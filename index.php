6dd55c4d5a709cf7dafa7447a5c18550fc07674e
EngGhada <ghadamohsenelsayed@gmail.com>
2025-01-09 19:01:25 +0100

    start new php project STUDI

--- index.php
+++ index.php
@@ -11,6 +11,11 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </head>
 <body>
+
+<?php
+echo 'test'.PHP_EOL;
+echo 'test';
+?>
   <div class="container">
     <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
       <a href="index.php" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
